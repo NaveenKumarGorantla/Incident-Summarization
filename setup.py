2b39fb9ae0cdb898dd7b62592d40b33c2ed90879
NaveenKumarGorantla <naveenkumargorantla9@gmail.com>
2021-03-09 03:19:56 +0000

    Code change

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
 	name='project0',
 	version='1.0',
-	author='You Name',
-	author_email='your ou email',
+	author='Naveen Kumar Gorantla',
+	author_email='Naveen.Kumar.Gorantla-1@ou.edu',
 	packages=find_packages(exclude=('tests', 'docs')),
 	setup_requires=['pytest-runner'],
 	tests_require=['pytest']	
